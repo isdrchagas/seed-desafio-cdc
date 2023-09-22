aa261d52a6b9908eb59587670233e9cb9a202891
isdrchagas <isagchagass@gmail.com>
2023-09-22 00:38:27 -0300

    setup do projeto

---
+++ src/test/java/cdc/dev/eficiente/cdc/CdcApplicationTests.java
@@ -0,0 +1,13 @@
+package cdc.dev.eficiente.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
